8622953b74ef7e230588f7828a5c496e233d153f
SaraPena <pena.e.sara@gmail.com>
2019-11-21 02:52:49 -0600

    Create viz.py file

---
+++ viz.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import prepare
+
+df = pd.read_csv('activity.csv')
